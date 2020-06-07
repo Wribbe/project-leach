9f9342b50d6653674149d9a6b90c5c83675fef24
Stefan Eng <eng_steff@hotmail.com>
2020-06-08 00:20:53 +0200

    Retab source, wierd..

--- src/main.c
+++ src/main.c
@@ -10,110 +10,110 @@
 char *
 file_read(const char * path)
 {
-	FILE * fh = fopen(path, "r");
-	if (fh == NULL) {
-		fprintf(stderr, "COULD NOT OPEN FILE %s\n.", path);
-		return NULL;
-	}
+  FILE * fh = fopen(path, "r");
+  if (fh == NULL) {
+    fprintf(stderr, "COULD NOT OPEN FILE %s\n.", path);
+    return NULL;
+  }
 
-	fseek(fh, 0, SEEK_END);
+  fseek(fh, 0, SEEK_END);
 
-	size_t size_string = ftell(fh);
-	rewind(fh);
+  size_t size_string = ftell(fh);
+  rewind(fh);
 
-	char * data = malloc(size_string);
-	if (data == NULL) {
-		fprintf(stderr, "COULD NOT ALLOCATE DATA\n.");
-		return NULL;
-	}
+  char * data = malloc(size_string);
+  if (data == NULL) {
+    fprintf(stderr, "COULD NOT ALLOCATE DATA\n.");
+    return NULL;
+  }
 
-	fread(data, 1, size_string, fh);
-	fclose(fh);
-	return data;
+  fread(data, 1, size_string, fh);
+  fclose(fh);
+  return data;
 }
 
 GLuint
 shaders_compile(void)
 {
-	GLint success = GL_FALSE;
+  GLint success = GL_FALSE;
 
-	char * src_shader_vertex = file_read("src/shaders/shader.vert");
-	char * src_shader_fragment = file_read("src/shaders/shader.frag");
+  char * src_shader_vertex = file_read("src/shaders/shader.vert");
+  char * src_shader_fragment = file_read("src/shaders/shader.frag");
 
-	// Create and compile vertex shader.
-	GLuint shader_vertex = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(shader_vertex, 1, (const char **)(&src_shader_vertex), NULL);
-	glCompileShader(shader_vertex);
-	glGetShaderiv(shader_vertex, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		fprintf(stderr, "Vertex shader failed to compile.\n");
-	}
+  // Create and compile vertex shader.
+  GLuint shader_vertex = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(shader_vertex, 1, (const char **)(&src_shader_vertex), NULL);
+  glCompileShader(shader_vertex);
+  glGetShaderiv(shader_vertex, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    fprintf(stderr, "Vertex shader failed to compile.\n");
+  }
 
-	// Create and compile fragment shader.
-	GLuint shader_fragment = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(shader_fragment, 1, (const char **)(&src_shader_fragment), NULL);
-	glCompileShader(shader_fragment);
-	glGetShaderiv(shader_fragment, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		fprintf(stderr, "Fragment shader failed to compile.\n");
-	}
+  // Create and compile fragment shader.
+  GLuint shader_fragment = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(shader_fragment, 1, (const char **)(&src_shader_fragment), NULL);
+  glCompileShader(shader_fragment);
+  glGetShaderiv(shader_fragment, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    fprintf(stderr, "Fragment shader failed to compile.\n");
+  }
 
-	// Create program, attatch shaders to it, ad link it.
-	GLuint program = glCreateProgram();
-	glAttachShader(program, shader_vertex);
-	glAttachShader(program, shader_fragment);
-	glLinkProgram(program);
+  // Create program, attatch shaders to it, ad link it.
+  GLuint program = glCreateProgram();
+  glAttachShader(program, shader_vertex);
+  glAttachShader(program, shader_fragment);
+  glLinkProgram(program);
 
-	// Delete the shaders as the program has them now.
-	glDeleteShader(shader_fragment);
-	glDeleteShader(shader_vertex);
+  // Delete the shaders as the program has them now.
+  glDeleteShader(shader_fragment);
+  glDeleteShader(shader_vertex);
 
   free(src_shader_vertex);
   free(src_shader_fragment);
 
-	return program;
+  return program;
 }
 
 void
 render(GLuint VAO, double time_current, GLuint program)
 {
-	GLfloat color[] = {
-		sinf(time_current) * 0.5f + 0.5f,
-		cosf(time_current) * 0.5f + 0.5f,
-		0.0f,
-		1.0f,
-	};
-	glClearBufferfv(GL_COLOR, 0, color);
-	glUseProgram(program);
+  GLfloat color[] = {
+    sinf(time_current) * 0.5f + 0.5f,
+    cosf(time_current) * 0.5f + 0.5f,
+    0.0f,
+    1.0f,
+  };
+  glClearBufferfv(GL_COLOR, 0, color);
+  glUseProgram(program);
   glBindVertexArray(VAO);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+  glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 int main(void)
 {
-	GLFWwindow* window;
+  GLFWwindow* window;
 
-	if (!glfwInit())
-		exit(EXIT_FAILURE);
+  if (!glfwInit())
+    exit(EXIT_FAILURE);
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
 
-	window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		exit(EXIT_FAILURE);
-	}
+  window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+  if (!window)
+  {
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
 
-	glfwMakeContextCurrent(window);
-	gladLoadGL();
-	glfwSwapInterval(1);
+  glfwMakeContextCurrent(window);
+  gladLoadGL();
+  glfwSwapInterval(1);
 
-	GLuint program_render = shaders_compile();
-	GLuint VAO = 0;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
+  GLuint program_render = shaders_compile();
+  GLuint VAO = 0;
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
 
   float vertices[] = {
     -0.5f, -0.5f, 0.0f,
@@ -129,15 +129,15 @@ int main(void)
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
 
-	while (!glfwWindowShouldClose(window))
-	{
-		glfwPollEvents();
-		render(VAO, glfwGetTime(), program_render);
-		glfwSwapBuffers(window);
-	}
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+    render(VAO, glfwGetTime(), program_render);
+    glfwSwapBuffers(window);
+  }
 
-	glfwDestroyWindow(window);
+  glfwDestroyWindow(window);
 
-	glfwTerminate();
-	exit(EXIT_SUCCESS);
+  glfwTerminate();
+  exit(EXIT_SUCCESS);
 }
