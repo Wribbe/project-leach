ba38731fca1f0b9b0e515f5f6e85324029612167
Stefan Eng <eng_steff@hotmail.com>
2020-06-08 00:20:18 +0200

    Successfully render triangle using buffers.

--- src/main.c
+++ src/main.c
@@ -75,7 +75,7 @@ shaders_compile(void)
 }
 
 void
-render(double time_current, GLuint program)
+render(GLuint VAO, double time_current, GLuint program)
 {
 	GLfloat color[] = {
 		sinf(time_current) * 0.5f + 0.5f,
@@ -85,6 +85,7 @@ render(double time_current, GLuint program)
 	};
 	glClearBufferfv(GL_COLOR, 0, color);
 	glUseProgram(program);
+  glBindVertexArray(VAO);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
@@ -111,13 +112,27 @@ int main(void)
 
 	GLuint program_render = shaders_compile();
 	GLuint VAO = 0;
-	glCreateVertexArrays(1, &VAO);
+	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
+
+  float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f,
+  };
+
+  GLuint VBO = 0;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
-		render(glfwGetTime(), program_render);
+		render(VAO, glfwGetTime(), program_render);
 		glfwSwapBuffers(window);
 	}
 
--- src/shaders/shader.vert
+++ src/shaders/shader.vert
@@ -1,14 +1,7 @@
 #version 450 core
+layout (location = 0) in vec3 aPos;
 
 void main(void)
 {
-
-	const vec4 vertices[3] = vec4[3](
-	 vec4( 0.25, -0.25, 0.5, 1.0),
-	 vec4(-0.25, -0.25, 0.5, 1.0),
-	 vec4( 0.25,  0.25, 0.5, 1.0)
-	);
-
-	gl_Position = vertices[gl_VertexID];
-
+	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);
 };
