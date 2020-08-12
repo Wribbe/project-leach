47e05ed680f2efdd2debae2a2307040a6b0a27f2
Stefan Eng <eng_steff@hotmail.com>
2020-08-12 22:20:18 +0200

    Finalize the file_read function.

--- lib/utils.c
+++ lib/utils.c
@@ -8,16 +8,41 @@ file_read(const char * filepath)
     fprintf(stderr, "Error: could not open file %s.\n", filepath);
     exit(EXIT_FAILURE);
   }
-  printf("Could open file: %s\n", filepath);
-  return NULL;
+  fseek(handle_file, 0, SEEK_END);
+  size_t size_file = ftell(handle_file);
+  rewind(handle_file);
+  char * data = malloc(size_file+1);
+  if (data == NULL) {
+    fprintf(
+      stderr,
+      "Error: Could not allocate enough data for reading file.\n"
+    );
+    exit(EXIT_FAILURE);
+  }
+  size_t size_read = fread(data, 1, size_file, handle_file);
+  if (size_read != size_file) {
+    fprintf(
+      stderr,
+      "Error: read-size: %zu, file-size %zu\n", size_read, size_file
+    );
+  }
+  data[size_file] = '\0';
+  return data;
 }
 
 
 GLuint
 program_create(const char * path_vert, const char * path_frag)
 {
-  UNUSED(path_vert);
-  UNUSED(path_frag);
+  char * data_vert = file_read(path_vert);
+  char * data_frag = file_read(path_frag);
+
+  printf("vert: %s\n", data_vert);
+  printf("frag: %s\n", data_frag);
+
+  free(data_vert);
+  free(data_frag);
+
   return 0;
 }
 
